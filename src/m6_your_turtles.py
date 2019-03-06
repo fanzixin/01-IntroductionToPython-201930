9c44d9d2ea06bef3e2c67dffd3eff4842f1c4d3e
fanz1 <fanz1@rose-hulman.edu>
2019-03-05 22:41:38 -0500

    Done

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Zixin Fan.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,3 +28,64 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+
+window=rg.TurtleWindow()
+window.tracer(10)
+
+c=rg.SimpleTurtle()
+c.pen=rg.Pen('green yellow',10)
+
+c.pen_up()
+c.go_to(rg.Point(-25,200))
+c.pen_down()
+
+c.right(135)
+
+for i in range(6):
+    for k in range(360):
+        c.left(1)
+        c.forward(0.4)
+    for j in range(15):
+        c.left(1)
+        c.forward(5)
+
+c.left(135)
+
+for i in range(4):
+    for j in range(15):
+        c.right(1)
+        c.forward(5)
+    for k in range(360):
+        c.right(1)
+        c.forward(0.3)
+
+
+d=rg.SimpleTurtle()
+d.pen=rg.Pen('blue',5)
+
+d.left(30)
+
+for k in range(2):
+    for i in range(360):
+        d.right(1)
+        d.forward(0.3)
+    d.right(120)
+
+d.right(90)
+
+for i in range(90):
+    d.right(1)
+    d.forward(0.3)
+
+d.pen_up()
+d.go_to(rg.Point(0,0))
+d.pen_down()
+
+d.left(90)
+
+for i in range(90):
+    d.left(1)
+    d.forward(0.3)
+
+window.close_on_mouse_click()
